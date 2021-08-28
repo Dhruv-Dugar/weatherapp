ad1fea3f18fdfa775798d454edf7ab18a7863363
codingthrupain <dhruvdugar02@gmail.com>
2021-08-28 19:48:06 +0530

    first commit

---
+++ Pods/Target Support Files/Pods-Weather-1/Pods-Weather-1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Weather_1 : NSObject
+@end
+@implementation PodsDummy_Pods_Weather_1
+@end
---
+++ Weather-1/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  Weather-1
+//
+//  Created by Dhurv Dugar on 27/08/21.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    
+    //connecting the objects of the UI with the code here
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+    //declaration of variables and apiKey
+    let gradientLayer = CAGradientLayer()
+    let apiKey = "90590ec508acf09ae262a3f19d2bf442"
+    var lat = 28.6447  //random meme level values
+    var lon = 77.2883   //why even bother if not using meme values
+    var activityIndicator: NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+    
+    
+    //functions go from here
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame: CGRect = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            setBlueGradientColour()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        let location = locations[0];
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImageView.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self.dayLabel.text = dateFormatter.string(from: date)
+                
+                let suffix = iconName.suffix(1)
+                if(suffix == "n"){
+                    self.setGreyGradientColour()
+                }else{
+                    self.setBlueGradientColour()
+                }
+
+            }
+        }
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func setBlueGradientColour(){
+        let topColor = UIColor(red: 95/255.0, green: 165/255.0, blue: 1/255.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72/255.0, green: 114/255.0, blue: 184/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [bottomColor, topColor]
+    }
+    
+    func setGreyGradientColour(){
+        let topColor = UIColor(red: 151/255.0, green: 151/255.0, blue: 151/255.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72/255.0, green: 72/255.0, blue: 72/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
+}
+
+
